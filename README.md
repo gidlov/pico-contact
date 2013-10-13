54c00e4e052b5705e986b244201111e16bc76262
gidlov <info@gidlov.com>
2013-10-13 17:01:55 +0200

    Release

---
+++ README.md
@@ -0,0 +1,99 @@
+A simple contact form for the micro CMS Pico
+=====================
+You may find more info on [gidlov.com/code/pico_contact](http://gidlov.com/code/pico_contact)
+
+This is a simple contact form for your Pico-project.
+
+##Example
+[Running example](http://gidlov.com/contact)
+
+##Setup
+We use the nice [PHPMailer](https://github.com/PHPMailer/PHPMailer) class in this plugin, so we have to update the `require` key of `composer.json` and add the following:
+
+	"phpmailer/phpmailer": "dev-master"
+
+Run the `composer update` comand.
+
+Now open your `config.php` file and add insert this:
+
+	$config['contact'] = array(
+		'post' => $_POST,
+		'send_to' => 'klas.gidlov@gmail.com',
+	);
+
+Create a contact.md page (or whatever you want to call it) and insert your contact form there, like this:
+
+	<!--CONTACT-MESSAGE-->
+	<form role="form" method="post" name="contact">
+		<div class="form-group contact-name-class">
+			<label for="name">Name*</label>
+			<input type="text" name="name" class="form-control" id="name" placeholder="Enter Name">
+		</div>
+		<div class="form-group contact-mail-class">
+			<label for="mail">Email address*</label>
+			<input type="email" class="form-control" id="mail" name="mail" placeholder="Enter email">
+		</div>
+		<div class="form-group contact-subject-class">
+			<label for="subject">Subject</label>
+			<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+		</div>
+		<div class="form-group contact-message-class">
+			<label for="message">Message*</label>
+			<textarea class="form-control" id="message" name="message" rows="5"></textarea>
+		</div>
+		<button type="submit" class="btn btn-default" name="contact" value="true">Submit</button>
+	</form>
+
+That's it!
+
+##Notice
+
+`<!--CONTACT-MESSAGE-->` at the top of the form is used to display messages, so leave it alone (or move it). Also leave the `name` attribute of the form element as it is, and the `name` and `value` of the submit button.
+The required fields are `namn`, `mail` and `message`.
+
+##Tweaks
+
+If your server is not configured to send mail, try to use SMTP instead by adding this to `config['contact']`:
+
+	'smtp' => array(
+		'host' => 'your_host',
+		'username' => 'your_username',
+		'password' => 'your_password',
+		'port' => 587,
+		'auth' => 'true',		// Enable SMTP authentication, true or false
+		'encryption' => 'tls',	// Enable encryption, tls or ssl
+	),
+
+Define a custom subject like this:
+
+	// %1$s = name, %2$s = mail, %3$s = input subject, %4$s = site_title, %5$s = base_url
+	'subject' => 'New mail from %1$s sent via %4$s', 
+
+Adding custom body data example:
+
+	// %1$s = name, %2$s = mail, %3$s = input subject, %4$s = site_title, %5$s = base_url
+	'body_header' => 'New mail from %4$s'."\n".'Name: %1$s'."\n".'Mail: %2$s'."\n".'Subject: %3$s'."\n".'Message:'."\n\n".'',
+	'body_footer' => '',
+
+Alert messages example:
+
+	'alert_messages' => array(
+		'error' => '<div class="uk-alert uk-alert-danger"><h2>Whops, error!</h2><p>Your message could not be sent. Sorry. %1$s</p></div>', //%1$s = PHPMailer error
+		'validation_error' => '<div class="uk-alert uk-alert-danger"><h2>Try again!</h2><p>Your message could not be sent.</p><p>%1$s</p></div>', //%1$s = Validation errors
+		'success' => '<div class="uk-alert uk-alert-success"><h2>Thanks for your message!</h2><p>I will reply as soon as possible.</p></div>',
+	),
+
+Validation messages example:
+
+	'validation_messages' => array(
+		'required' => '', 		// %1$s = field name
+		'invalid_mail' => '',	// %1$s = field name
+	),
+
+A custom form input validation error class:
+
+	'error_class' => 'form-danger',
+
+##License
+
+Contact is released under [LGPL](http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
---
+++ contact.php
@@ -0,0 +1,116 @@
+<?php use \PHPMailer;
+
+/**
+ * Simple contact form for the micro CMS Pico.
+ *
+ * @author Klas Gidlöv
+ * @link http://gidlov.com/code/
+ * @license LGPL
+ */
+
+define('CONTACT_MESSAGE', '<!--CONTACT-MESSAGE-->');
+
+class Contact {
+
+	private $validation;
+	private $message;
+	private $error;
+	private $post;
+
+	public function config_loaded(&$settings) {
+		// Missing config settings.
+		if (empty($settings['contact']))
+			return;
+		// No post request.
+		if (empty($settings['contact']['post']))
+			return;
+		$this->contact = $settings['contact'];
+		$this->post = $settings['contact']['post'];
+
+		// Post to this form was made.
+		if (isset($this->post['contact']) AND $this->post['contact'] == 'true') {
+			foreach (array('name', 'mail', 'message') as $value) {
+				if (empty($this->post[$value])) {
+					$this->validation[$value] = isset($this->contact['validation_messages']['required']) ? sprintf($this->contact['validation_messages']['required'], $value) : "The {$value}-feild is required.";
+				}
+				if ($value == 'mail') {
+					if (filter_var($this->post['mail'], FILTER_VALIDATE_EMAIL) === false) {
+						$this->validation[$value] = isset($this->contact['validation_messages']['invalid_mail']) ? sprintf($this->contact['validation_messages']['invalid_mail'], $value) : "A valid {$value} i required.";;
+					}
+				}
+			}
+		}
+		// No validation validation, proceed sending the email.
+		if (count($this->validation) == 0) {
+			$mail = new \PHPMailer;
+			if (isset($this->contact['smtp'])) {
+				$mail->isSMTP();
+				$mail->Host = $this->contact['smtp']['host'] ? $this->contact['smtp']['host'] : '';
+				$mail->SMTPAuth = $this->contact['smtp']['auth'] ? $this->contact['smtp']['auth'] : '';
+				$mail->Username = $this->contact['smtp']['username'] ? $this->contact['smtp']['username'] : '';
+				$mail->Password = $this->contact['smtp']['password'] ? $this->contact['smtp']['password'] : '';
+				$mail->SMTPSecure = $this->contact['smtp']['encryption'] ? $this->contact['smtp']['encryption'] : '';
+				$mail->Port = $this->contact['smtp']['port'] ? $this->contact['smtp']['port'] : '';
+			}
+			$mail->FromName = $this->post['name'];
+			$mail->From = $this->post['mail'];
+			$mail->addAddress($this->contact['send_to']);
+			$subject = isset($this->post['subject']) ? $this->post['subject'] : '';
+			$args = array($this->post['name'], $this->post['mail'], $subject, $settings['site_title'], $settings['base_url']);
+			$header = isset($this->contact['body_header']) ? vsprintf($this->contact['body_header'], $args) : '';
+			$footer = isset($this->contact['body_footer']) ? vsprintf($this->contact['body_footer'], $args) : '';
+			if (isset($this->contact['subject'])) {
+				$mail->Subject = vsprintf($this->contact['subject'], $args);
+			} elseif ($subject != '') {
+				$mail->Subject = $this->post['subject'];
+			}
+			$mail->Body = $header.$this->post['message'].$footer;
+			
+			if(!$mail->send()) {
+				// Try to use SMTP if you'll get here.
+				$this->error = isset($mail->validationInfo) ? $mail->validationInfo : 'Unknown mailing error.';
+			} else {
+				$this->post = false;
+				$this->message = true;
+			}
+		}
+	}
+
+	public function content_parsed(&$content) {
+		// Show validation failiurs.
+		if (isset($this->validation)) {
+			$validation = '';
+				foreach ($this->validation as $section => $value) {
+					if (isset($value) AND $value != '') {
+						if (isset($this->contact['error_class'])) {
+							$content = preg_replace('/<input(.*?name="'.$section.'".*?class=".*?)"/ms', '<input$1 '.$this->contact['error_class'].'"', $content);
+						}
+						$validation .= $value."<br />\n";
+					}
+				}
+			if ($validation) {
+				$message = isset($this->contact['alert_messages']['validation_error']) ? $this->contact['alert_messages']['validation_error'] : '<div class="alert alert-danger"><h4>Validation Failed!</h4><p>%1$s</p></div>';
+				$content = preg_replace('/'.CONTACT_MESSAGE.'/ms', sprintf($message, $validation), $content);
+			}
+		}
+		if ($this->message) {
+			$message = isset($this->contact['alert_messages']['success']) ? $this->contact['alert_messages']['success'] : '<div class="alert alert-success"><h2>Thanks for your message!</h2><p>I will reply as soon as possible.</p></div>';
+			$content = preg_replace('/'.CONTACT_MESSAGE.'/ms', $message, $content);
+		}
+		if ($this->error) {
+			$message = isset($this->contact['alert_messages']['error']) ? $this->contact['alert_messages']['error'] : '<div class="alert alert-danger"><h2>Whops, error!</h2><p>Your message could not be sent. Sorry. %1$s</p></div>';
+			$content = preg_replace('/'.CONTACT_MESSAGE.'/ms', sprintf($message, $this->error), $content);
+		}
+		// User input.
+		if (empty($this->post))
+			return;
+		foreach ($this->post as $key => $value) {
+			if ($key == 'message') {
+				$content = preg_replace('/<textarea(.*?)><\/textarea>/', '<textarea$1>'.$value.'</textarea>', $content);	
+			} else {
+				$content = preg_replace('/<input(.*?)name="'.$key.'"/ms', "<input$1name='{$key}' value='{$value}'", $content);
+			}
+		}
+	}
+
+}
